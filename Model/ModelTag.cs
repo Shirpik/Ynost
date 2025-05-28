ee13bfbc209a8d3a9817efdceeca26f4393f114d
stfy <teamft2005@gmail.com>
2025-05-29 00:26:38 +0500

    2

--- Model/ModelTag.cs
+++ Model/ModelTag.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Ynost.Model
 {
-    internal class ModelTag: ObservableObject
+    public partial class ModelTag : ObservableObject
     {
-
+        [ObservableProperty]
+        private string[] _titles = {}
     }
 }
